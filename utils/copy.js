e4846a025379c8d2ffd96b9a573b0673a77c9480
SirCaptainMitch <Hamann.Mitchell@gmail.com>
2016-06-28 18:31:22 -0500

    initial commit

---
+++ utils/copy.js
@@ -0,0 +1,32 @@
+/**
+ * Created by SirCaptainMitch on 6/28/2016.
+ */
+var copy = require('recursive-copy');
+
+var options ={
+    overwrite: true,
+    expand: true,
+    dot: true,
+    junk: true,
+    filter: [
+        '**/*.html',
+        '**/*.css'
+    ]
+}
+
+copy('./app', './client', options)
+    .on(copy.events.COPY_FILE_START, function(copyOperation) {
+        console.info('Copying file ' + copyOperation.src + '...');
+    })
+    .on(copy.events.COPY_FILE_COMPLETE, function(copyOperation) {
+        console.info('Copied to ' + copyOperation.dest);
+    })
+    .on(copy.events.ERROR, function(error, copyOperation) {
+        console.error('Unable to copy ' + copyOperation.dest);
+    })
+    .then(function(results) {
+        console.info(results.length + ' file(s) copied');
+    })
+    .catch(function(error) {
+        return console.error('Copy failed: ' + error);
+    });
